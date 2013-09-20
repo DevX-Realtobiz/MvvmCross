e70effd695c445113ee0f490ba05da1f860ffe6c
Tim Uy <tim@loqu8.com>
2013-09-20 14:44:08 -0700

    Mod to additionally match *ViewController to *ViewModel for iOS and Mac

--- Samples/FirstDemo2.Mac/Views/FirstView.cs
+++ Samples/FirstDemo2.Mac/Views/FirstView.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using MonoMac.Foundation;
 using MonoMac.AppKit;
-using Cirrious.MvvmCross.Mac.Views;
-using Cirrious.MvvmCross.Binding.BindingContext;
-using FirstDemo.Core.ViewModels;
+using Cirrious.MvvmCross.Binding.Mac.Views;
 
 namespace FirstDemo2.Mac
 {
-	public partial class FirstView : MvxViewController
+	public partial class FirstView : MvxView
 	{
 		#region Constructors
 		// Called when created from unmanaged code
@@ -23,40 +21,11 @@ namespace FirstDemo2.Mac
 		{
 			Initialize ();
 		}
-		// Call to load from the XIB/NIB file
-		public FirstView () : base ("SecondView", NSBundle.MainBundle)
-		{
-			Initialize ();
-		}
 		// Shared initialization code
 		void Initialize ()
 		{
 		}
 		#endregion
-		//strongly typed view accessor
-		public new SecondView View {
-			get {
-				return (SecondView)base.View;
-			}
-		}
-
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
-
-			var textEditFirst = new NSTextField(new System.Drawing.RectangleF(0,0,320,40));
-			View.AddSubview (textEditFirst);
-			var textEditSecond = new NSTextField(new System.Drawing.RectangleF(0,50,320,40));
-			View.AddSubview(textEditSecond);
-			var labelFull = new NSTextField(new System.Drawing.RectangleF(0,100,320,40));
-			View.AddSubview (labelFull);
-
-			var set = this.CreateBindingSet<FirstView, FirstViewModel> ();
-			set.Bind (textEditFirst).For(v => v.StringValue).To (vm => vm.FirstName);
-			set.Bind (textEditSecond).For(v => v.StringValue).To (vm => vm.LastName);
-			set.Bind (labelFull).For(v => v.StringValue).To (vm => vm.FullName);	
-			set.Apply ();
-		}
 	}
 }
 
